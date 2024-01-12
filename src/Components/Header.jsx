fc487c7a2c1f088b47b821329af6527786e3df92
shiransh <sp999pratap@gmail.com>
2024-01-12 14:18:24 +0530

    changes in Buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,29 @@
-import { Button, HStack } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button, HStack } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const buttonStyle = {
+    variant: "unstyled",
+    color: "black",
+    border: "1px solid black",
+    marginRight: "10px",
+  };
   return (
     <div>
       <HStack p={"4"} shadow={"base"} bgColor={"blue.800"}>
-        <Button variant={"unstyled"} color={"white"}>
+        <Button style={buttonStyle}>
           <Link to="/">Home</Link>
         </Button>
-        <Button variant={"unstyled"} color={"white"}>
+        <Button style={buttonStyle}>
           <Link to="/exchanges">Exchanges</Link>
         </Button>
-        <Button variant={"unstyled"} color={"white"}>
+        <Button style={buttonStyle}>
           <Link to="/coins">Coins</Link>
         </Button>
       </HStack>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
